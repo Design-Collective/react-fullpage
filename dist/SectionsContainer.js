75532e98b2a9e3446e54774dbf0285ed548f0d1e
sugarskull <rodrigo@designcollective.io>
2017-08-30 20:55:53 -0700

    adds nav anchor names

--- dist/SectionsContainer.js
+++ dist/SectionsContainer.js
@@ -371,8 +371,13 @@ var SectionsContainer = function (_React$Component) {
                     transform: _this5.state.activeSection === index ? 'scale(1.3)' : 'none'
                 };
 
-                return React.createElement('a', { href: '#' + link, key: index, className: _this5.props.navigationAnchorClass || 'Navigation-Anchor',
-                    style: _this5.props.navigationAnchorClass ? null : anchorStyle });
+                return React.createElement(
+                    'a',
+                    { href: '#' + link, key: index,
+                        className: _this5.props.navigationAnchorClass || 'Navigation-Anchor',
+                        style: _this5.props.navigationAnchorClass ? null : anchorStyle },
+                    _this5.props.navigationTooltips[index]
+                );
             });
 
             return React.createElement(
